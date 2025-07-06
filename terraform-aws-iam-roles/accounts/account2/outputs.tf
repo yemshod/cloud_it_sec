cc23e3c2ea8f84faef8466e105ac4830a36390ba
yemshod <159788497+yemshod@users.noreply.github.com>
2025-07-05 19:41:52 -0500

    Delete terraform-aws-iam-roles/accounts directory

--- terraform-aws-iam-roles/accounts/account2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "roles" {
-  description = "Map of IAM roles created"
-  value       = module.iam_roles.roles
-}
-
-output "permission_boundaries" {
-  description = "Map of permission boundary policies created"
-  value       = module.iam_roles.permission_boundaries
-}
